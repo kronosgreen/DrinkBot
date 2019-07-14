690dc885fa163fa3e57c8999f0427aa4b0a5a5a8
Christopher Medrano-Berumen <cmedranoberumen2844@floridapoly.edu>
2019-07-13 20:30:43 -0400

    can input ingredients now

--- getRecipes.py
+++ getRecipes.py
@@ -20,18 +20,26 @@ import json
 # in rasberry pi
 # driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver")
 
+# Collect available ingredients
 f = open("ingredients.json")
 ing_file = json.load(f)
 ingredients = ing_file['ingredients']
 
+# Open website
 driver = webdriver.Chrome()
-driver.get("www.cocktailbuilder.com")
+driver.get("https://www.cocktailbuilder.com/")
 
-print(driver.title)
+assert driver.title == "Cocktail Builder: Mixed Drinks From What You Already Have"
 
+# counter for ingredients added
+i = 0
 for ingredient in ingredients:
-    elem = driver.find_element_by_id("addingredient")
-    elem.send_keys(ingredient)
-    elem.send_keys(Keys.RETURN)
+    add_ingredient = driver.find_element_by_id("addIngredient")
+    add_ingredient.send_keys(ingredient)
+    found_ingredients = driver.find_elements_by_class_name('tt-suggestion')
+    found_ingredients[0].click()
+    i = i + 1
+    if i == 5:
+        driver.find_element_by_class_name("noThanks").find_element_by_tag_name("button").click()
 
-driver.close()
+#driver.close()
